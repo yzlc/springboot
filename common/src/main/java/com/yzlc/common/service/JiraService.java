30f9f52d7884fe64a2f2e33cf14d52432609644c
yzlc <yzlc233@outlook.com>
2023-05-22 22:30:50 +0800

    1

--- common/src/main/java/com/yzlc/common/service/JiraService.java
+++
@@ -1,8 +0,0 @@
-package com.yzlc.common.service;
-
-import java.io.IOException;
-import java.util.Map;
-
-public interface JiraService {
-    void createIssue(Map<String, Object> map) throws IOException;
-}
