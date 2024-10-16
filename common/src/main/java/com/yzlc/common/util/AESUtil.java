01089b79c85b0bdfeb5160c4dbbf09c5945660e7
lim <yzlc233@outlook.com>
2024-10-16 10:32:35 +0800

    1

--- common/src/main/java/com/yzlc/common/util/AESUtil.java
+++ common/src/main/java/com/yzlc/common/util/AESUtil.java
@@ -1,14 +1,12 @@
 package com.yzlc.common.util;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.util.Base64;
 
 /**
  * AES加密工具类
@@ -26,7 +24,7 @@ public class AESUtil {
         Cipher cipher = Cipher.getInstance(AES_CBC_ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, key,iv);
         byte[] encrypted = cipher.doFinal(value.getBytes());
-        return new BASE64Encoder().encode(encrypted);
+        return Base64.getEncoder().encodeToString(encrypted);
     }
 
     public static String decrypt(String encrypted) throws GeneralSecurityException, IOException {
@@ -34,7 +32,7 @@ public class AESUtil {
         SecretKeySpec key = new SecretKeySpec(KEY.getBytes(StandardCharsets.UTF_8), AES);
         Cipher cipher = Cipher.getInstance(AES_CBC_ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, key,iv);
-        byte[] original = cipher.doFinal(new BASE64Decoder().decodeBuffer(encrypted));
+        byte[] original = cipher.doFinal(Base64.getDecoder().decode(encrypted));
         return new String(original);
     }
 }
